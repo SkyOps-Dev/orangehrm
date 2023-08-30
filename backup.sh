9460b2c65728dd0b83d8aacf9c7f79f118945d86
syedmusaali359 <115637617+syedmusaali359@users.noreply.github.com>
2023-08-30 12:54:36 +0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -11,6 +11,10 @@ DB_USER="orange1"
 DB_PASSWORD="orange1"
 DB_NAME="orange1"
 
+# Maximum number of backups to retain in S3
+MAX_BACKUPS=5
+
+
 # S3 bucket name
 S3_BUCKET="orangehrm-backup"
 
@@ -31,14 +35,29 @@ if [ $? -eq 0 ]; then
   echo "Database backup completed successfully. Backup file: ${BACKUP_DIR}/${BACKUP_FILENAME}"
    # Upload the backup file to S3
   aws s3 cp "${BACKUP_DIR}/${BACKUP_FILENAME}" "s3://${S3_BUCKET}/${BACKUP_FILENAME}"
-  
+
   if [ $? -eq 0 ]; then
     echo "Backup file uploaded to S3: s3://${S3_BUCKET}/${BACKUP_FILENAME}"
+    
+    # Delete the oldest backup in S3 if there are more than ${MAX_BACKUPS}
+    BACKUP_LIST=$(aws s3 ls "s3://${S3_BUCKET}/" | awk '{print $4}' | sort)
+    BACKUP_COUNT=$(echo "${BACKUP_LIST}" | wc -l)
+    
+    if [ ${BACKUP_COUNT} -gt ${MAX_BACKUPS} ]; then
+      EXCESS_BACKUPS=$(echo "${BACKUP_LIST}" | head -n $((BACKUP_COUNT - MAX_BACKUPS)))
+      for BACKUP in ${EXCESS_BACKUPS}; do
+        aws s3 rm "s3://${S3_BUCKET}/${BACKUP}"
+        echo "Excess backup deleted from S3: ${BACKUP}"
+      done
+    fi
+    
   else
     echo "Failed to upload backup file to S3."
   fi
+
 else
   echo "Database backup failed."
 fi
+
 # Schedule the script to run again using cron job
 (crontab -l ; echo "0 18 * * 1-5 /bin/bash /home/ubuntu/orangehrm/backup.sh") | crontab -
