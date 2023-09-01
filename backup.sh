a7a0040295a2e5010a931e7f9e5f2b468c7c332c
syedmusaali359 <115637617+syedmusaali359@users.noreply.github.com>
2023-09-01 10:52:41 +0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Define the backup directory on the host machine
-BACKUP_DIR="/var/lib/docker/volumes"
-
 # MySQL container name
 MYSQL_CONTAINER="orange-mysql"
 
@@ -13,7 +10,6 @@ DB_NAME="orange1"
 
 # Maximum number of backups to retain in S3
 MAX_BACKUPS=5
-
 
 # S3 bucket name
 S3_BUCKET="orangehrm-backup"
@@ -27,18 +23,13 @@ BACKUP_FILENAME="${DB_NAME}_backup_${TIMESTAMP}.sql"
 # Grant PROCESS privilege to the user
 docker exec -i ${MYSQL_CONTAINER} mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT PROCESS ON *.* TO 'orange1'@'%'"
 
-# Run the mysqldump command inside the MySQL container
-docker exec -i ${MYSQL_CONTAINER} mysqldump -u${DB_USER} -p"${DB_PASSWORD}" ${DB_NAME} > "${BACKUP_DIR}/${BACKUP_FILENAME}"
+# Run the mysqldump command inside the MySQL container and upload directly to S3
+docker exec -i ${MYSQL_CONTAINER} mysqldump -u${DB_USER} -p"${DB_PASSWORD}" ${DB_NAME} | aws s3 cp - "s3://${S3_BUCKET}/${BACKUP_FILENAME}"
 
 # Check if the backup was successful
 if [ $? -eq 0 ]; then
-  echo "Database backup completed successfully. Backup file: ${BACKUP_DIR}/${BACKUP_FILENAME}"
-   # Upload the backup file to S3
-  aws s3 cp "${BACKUP_DIR}/${BACKUP_FILENAME}" "s3://${S3_BUCKET}/${BACKUP_FILENAME}"
+  echo "Database backup completed successfully. Backup file uploaded to S3: s3://${S3_BUCKET}/${BACKUP_FILENAME}"
 
-  if [ $? -eq 0 ]; then
-    echo "Backup file uploaded to S3: s3://${S3_BUCKET}/${BACKUP_FILENAME}"
-    
     # Delete the oldest backup in S3 if there are more than ${MAX_BACKUPS}
     BACKUP_LIST=$(aws s3 ls "s3://${S3_BUCKET}/" | awk '{print $4}' | sort)
     BACKUP_COUNT=$(echo "${BACKUP_LIST}" | wc -l)
@@ -49,15 +40,9 @@ if [ $? -eq 0 ]; then
         aws s3 rm "s3://${S3_BUCKET}/${BACKUP}"
         echo "Excess backup deleted from S3: ${BACKUP}"
       done
-    fi
-    
-  else
-    echo "Failed to upload backup file to S3."
-  fi
-
+    fi    
 else
-  echo "Database backup failed."
+  echo "Failed to upload backup file to S3."
 fi
-
 # Schedule the script to run again using cron job
 (crontab -l ; echo "0 18 * * 1-5 /bin/bash /home/ubuntu/orangehrm/backup.sh") | crontab -
